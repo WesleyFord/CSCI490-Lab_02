25bc80b161d4e5eba5a056fc210cff8e76b5c33c
WesleyFord <wesley.ford803@gmail.com>
2021-02-18 18:23:36 -0500

    create project

--- app/src/main/java/com/example/intentdemo/MainActivity.java
+++ app/src/main/java/com/example/intentdemo/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Intent i;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.plain_text_input);
+
+        button = findViewById(R.id.button);
+        i = new Intent(this, SecondActivity.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                i.putExtra("testString", s);
+
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/intentdemo/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.intentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent i = getIntent();
+        String s = i.getExtras().getString("testString");
+
+        textView2 = findViewById(R.id.textView2);
+
+        textView2.setText(s);
+
+        finish();
+    }
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
+}
