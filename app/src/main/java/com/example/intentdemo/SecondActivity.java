fda6d9857eb62b0359f6159f6a622b2ff897f21b
WesleyFord <wesley.ford803@gmail.com>
2021-02-23 18:51:57 -0500

    Did Part 2

--- app/src/main/java/com/example/intentdemo/SecondActivity.java
+++ app/src/main/java/com/example/intentdemo/SecondActivity.java
@@ -1,26 +1,63 @@
 package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-    TextView textView2;
+    ImageView moon, waterfall;
+    int imageID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_second);
-
         Intent i = getIntent();
-        String s = i.getExtras().getString("testString");
 
-        textView2 = findViewById(R.id.textView2);
+        moon = findViewById(R.id.moon);
+        waterfall = findViewById(R.id.waterfall);
 
-        textView2.setText(s);
+        moon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageID = R.drawable.supermoon;
+                finish();
+            }
+        });
+        waterfall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageID = R.drawable.waterfall;
+                finish();
+            }
+        });
 
+//        Activity 1 Below
+//
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_second);
+//
+//        Intent i = getIntent();
+//        String s = i.getExtras().getString("testString");
+//
+//        textView2 = findViewById(R.id.textView2);
+//
+//        textView2.setText(s);
+
+    }
+
+    @Override
+    public void finish() {
+        Intent i = new Intent();
+        i.putExtra("imageID",imageID);
+
+        setResult(RESULT_OK, i);
+        super.finish();
     }
 }
